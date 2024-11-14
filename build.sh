962eba79b710263f9d8db8028e09fbaaf5270ebf
Hamish Macpherson <hamstu@gmail.com>
2024-11-13 22:59:54 -0800

    fix paths on windows and in compiled binary

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 rm -rf release
 deno compile --allow-read --allow-write --target x86_64-apple-darwin --output release/mpcfill-custom-cards-macos-intel main.ts
 deno compile --allow-read --allow-write --target aarch64-apple-darwin --output release/mpcfill-custom-cards-macos-arm main.ts
-deno compile --allow-read --allow-write --target x86_64-pc-windows-msvc --output release/mpcfill-custom-cards-macos main.ts
+deno compile --allow-read --allow-write --target x86_64-pc-windows-msvc --output release/mpcfill-custom-cards main.ts
